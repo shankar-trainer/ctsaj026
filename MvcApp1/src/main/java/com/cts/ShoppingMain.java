5561fe1068c16888c5d1731e20b8e177d5da73d5
shankar kumar <shankar7979@hhotmail.com>
2021-05-24 18:31:40 +0530

    mvc commit2

--- MvcApp1/src/main/java/com/cts/ShoppingMain.java
+++ MvcApp1/src/main/java/com/cts/ShoppingMain.java
@@ -1,14 +1,19 @@
 package com.cts;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShoppingMain {
+public class ShoppingMain implements CommandLineRunner {
 	
 	public static void main(String[] args) {
 	
 		SpringApplication.run(ShoppingMain.class, args);	
+	}
+
+	public void run(String... args) throws Exception {
+		System.out.println("run can contain additional code");
 	}
 	
 }
